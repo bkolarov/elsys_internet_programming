6a3b44333e529ec5e2c88f22e240f345aa90be0b
crash-id <kbogomil@gmail.com>
2014-12-31 14:01:26 +0200

    Task 4: done

--- JQuery_HW02/jQuery.js
+++ JQuery_HW02/jQuery.js
@@ -7,4 +7,7 @@ $(function() {
 	console.log('Task 3');
 	console.log($('.inscreen').children('div').first().children('p').text());
 
+	//task 4
+	console.log('Task 4');
+	$("#menu-top-level-menu").append('<li><a>new button</a></li>');
 });
