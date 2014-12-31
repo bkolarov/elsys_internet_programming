8490ed4c7ccef2cea8da7eb25de0be7b5f36f802
crash-id <kbogomil@gmail.com>
2014-12-31 13:52:55 +0200

    Task 3: done

--- JQuery_HW02/jQuery.js
+++ JQuery_HW02/jQuery.js
@@ -1,4 +1,10 @@
 $(function() { 
-	console.log($('#footer').children('a').attr('title'));
+	//task 2
+	console.log('Task 2');
+	console.log($('#footer').children('a').first().attr('title'));
+
+	//task 3
+	console.log('Task 3');
+	console.log($('.inscreen').children('div').first().children('p').text());
 
 });
